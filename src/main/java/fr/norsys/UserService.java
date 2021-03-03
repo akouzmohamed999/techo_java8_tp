213df9d3a8203e05e2c0aae7d4c72c41ec24c8cb
Mohamed AKOUZ <makouz@norsys.fr>
2021-03-03 22:01:22 +0100

    initial commit

---
+++ src/main/java/fr/norsys/UserService.java
@@ -0,0 +1,43 @@
+package fr.norsys;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+
+    public Optional<User> getUserValue(User user) {
+        return null;
+    }
+
+    public User throwExceptionIfUserIsNull(User user) {
+        return null;
+    }
+
+    public User getUserUsingOrElse(User user) {
+        return null;
+    }
+
+    public String getUserEmail(User user) {
+        return null;
+    }
+
+    public Boolean isUserEmailValid(User user) {
+        return null;
+    }
+
+    public String getUserCountryName(User user) {
+        return null;
+    }
+
+    public String getFirstUserEmail(User user1, User user2) throws Exception {
+        return null;
+    }
+
+    public List<User> getListFromUser(User user) {
+        return null;
+    }
+
+    public List<User> getListFromUserBis(User user) {
+        return null;
+    }
+}
